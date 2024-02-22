9fa38b6883b4e3fb64ab78ca766f95d4e5f80c3a
kwi5124 <kwi5124@naver.com>
2024-02-22 20:03:16 +0900

    멤버 로그인, 회원가입, 이메일인증, 비번아이디인증, 자동저장

--- src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -14,27 +14,13 @@ public class RequestJoin {
 
     private String mode = "step1";
 
-    @NotBlank
     @Email
     private String email;
 
-    @NotBlank
-    @Size(min=6)
-    private String userId;
-
-    @Size(min=8)
-    private String password;
-
-    @NotBlank
-    private String confirmPassword;
-
-    @NotBlank
     private String name;
 
-    @AssertTrue
     private boolean agree; //개인정보 동의 (필수)
 
-    @AssertTrue
     private boolean agree2;  // 개인정보 필수항목에 대한 처리 및 이용(필수)
 
     private boolean agree3;  // 개인정보 선택항목에 대한 처리 및 이용
@@ -43,21 +29,26 @@ public class RequestJoin {
 
     private boolean agree5;  // 개인정보의 위탁
 
+    @Size(min=6)
+    private String userId;
+
+    @Size(min=8)
+    private String password;
+
+    private String confirmPassword;
+
     private boolean SMSAgree;
 
     private boolean EmailAgree;
 
-    @NotBlank
     @Size(min=10, max=11)
     private String tel;
 
-    @NotBlank
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private String birth;
 
     private String gender;
 
     private String authority;
-
 
 }
