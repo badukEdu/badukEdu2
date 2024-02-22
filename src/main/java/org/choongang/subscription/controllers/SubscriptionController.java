b36df9b9d35e07d3f644c2fad54ef0184d7cecf9
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-02-22 17:44:14 +0900

    게임컨텐츠 등록, 수정, 삭제, 검색, 구독서비스(진행중)

--- src/main/java/org/choongang/subscription/controllers/SubscriptionController.java
+++ src/main/java/org/choongang/subscription/controllers/SubscriptionController.java
@@ -1,12 +1,14 @@
 package org.choongang.subscription.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.admin.gamecontent.controllers.GameContentSearch;
+import org.choongang.admin.gamecontent.entities.GameContent;
+import org.choongang.admin.gamecontent.service.GameContentInfoService;
+import org.choongang.commons.ListData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,27 +18,52 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SubscriptionController {
 
+    private final GameContentInfoService gameContentInfoService;
+
     /* 구매한 게임콘텐츠 목록 */
     @GetMapping
-    public String list(Model model) {
+    public String list(@ModelAttribute GameContentSearch search,
+                       Model model) {
         commonProcess("list", model);
+
+        ListData<GameContent> data = gameContentInfoService.getList(search);
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());;
 
         return "subscription/list";
     }
 
     /* 게임컨텐츠 구매 할 수 있는 리스트*/
     @GetMapping("/subscribe")
-    public String subscribe(Model model) {
+    public String subscribe(@ModelAttribute GameContent form,
+                            GameContentSearch search,
+                            Model model) {
         commonProcess("apply", model);
+
+        ListData<GameContent> data = gameContentInfoService.getList(search);
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
 
         return "subscription/subscribe";
     }
 
-    @PostMapping("/subscribe/apply")
-    public String subscribePs(Model model) {
+    /* 게임컨텐츠 결제 */
+    @PostMapping("/apply")
+    public String subscribePs(@ModelAttribute GameContentSearch search, Model model,
+                              @RequestParam(name = "chk") List<Long> chks) {
         commonProcess("apply", model);
 
-        return "redirect:/subscription";
+        List<GameContent> items = new ArrayList<>();
+        Long totalMoney =0L;
+        for(Long num : chks){
+            items.add(gameContentInfoService.getById(num));
+            totalMoney += gameContentInfoService.getById(num).getSalePrice();
+        }
+
+        model.addAttribute("totalMoney", totalMoney);
+        model.addAttribute("items", items);
+
+        return "subscription/list";
     }
 
     private void commonProcess(String mode, Model model) {
