87f63321bbd0bcce12a132d79224e211b12c47d6
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-02-22 17:59:54 +0900

    학습자료 등록(진행중)

--- src/main/java/org/choongang/admin/education/controllers/EducationController.java
+++ src/main/java/org/choongang/admin/education/controllers/EducationController.java
@@ -1,13 +1,17 @@
 package org.choongang.admin.education.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.choongang.admin.education.entities.EduData;
+import org.choongang.admin.education.service.EduContentDeleteService;
+import org.choongang.admin.education.service.EduDataInfoService;
+import org.choongang.admin.education.service.EduDataSaveService;
+import org.choongang.commons.ListData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,29 +21,69 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EducationController {
 
+    private final EduDataSaveService eduDataSaveService;
+    private final EduDataInfoService eduDataInfoService;
+    private final EduContentDeleteService eduContentDeleteService;
+
     @GetMapping
-    public String list(Model model) {
+    public String list(@ModelAttribute EduDataSearch search, Model model) {
         commonProcess("list", model);
+
+        ListData<EduData> data = eduDataInfoService.getList(search);
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
 
         return "admin/education/list";
     }
 
+    /**
+     * 교육 자료 등록
+     *
+     * @param form
+     * @param model
+     * @return
+     */
     @GetMapping("/add")
-    public String add(Model model) {
+    public String add(@ModelAttribute RequestEduData form, Model model) {
         commonProcess("add", model);
 
         return "admin/education/add";
     }
 
+    /**
+     * 교육 자료 수정
+     *
+     * @param num
+     * @param model
+     * @return
+     */
     @GetMapping("/edit/{num}")
     public String edit(@PathVariable("num") Long num, Model model) {
         commonProcess("edit", model);
 
+        RequestEduData form = eduDataInfoService.getForm(num);
+        model.addAttribute("requestEduData", form);
+
         return "admin/education/edit";
     }
 
+    @GetMapping("/delete/{num}")
+    public String delete(@PathVariable("num") Long num, Model model) {
+        eduContentDeleteService.delete(num);
+
+        return "redirect:/admin/edu/list";
+    }
+
     @PostMapping("/save")
-    public String save(Model model) {
+    public String save(@Valid RequestEduData form, Errors errors, Model model) {
+        String mode = form.getMode();
+        commonProcess(mode, model);
+
+        if(errors.hasErrors()) {
+            return "admin/education/" + mode;
+        }
+
+        eduDataSaveService.save(form);
 
         return "redirect:/admin/education";
     }
