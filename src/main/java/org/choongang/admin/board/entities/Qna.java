558d41a294e50d5fec5c4f536db9af60bee9c400
hobe0m <beom231231@gmail.com>
2024-02-22 18:05:05 +0900

    Board 수정(2)

---
+++ src/main/java/org/choongang/admin/board/entities/Qna.java
@@ -0,0 +1,29 @@
+package org.choongang.admin.board.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Qna {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "qna_seq")
+    @SequenceGenerator(name = "qna_seq", sequenceName = "QNA_SEQ", allocationSize = 1)
+    private Long num;
+
+    private String type;
+
+    private String title;
+
+    private String answerAlert;
+
+    private String content;
+
+    @Column
+    private String fileName; // 파일명 (파일명)
+
+    @Column
+    private String fileAddress; // 파일경로 (파일 경로)
+
+}
