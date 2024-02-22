7020727feadc7f5e7d3c6159b2a0e3a0e3f8f188
lgh <dlrlgmd25@gmail.com>
2024-02-22 15:25:03 +0900

    group 프론트 완료

--- src/main/java/org/choongang/member/entities/Member.java
+++ src/main/java/org/choongang/member/entities/Member.java
@@ -2,34 +2,89 @@ package org.choongang.member.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 import org.choongang.commons.entities.Base;
 import org.choongang.member.Authority;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 public class Member extends Base {
-    @Id @GeneratedValue
-    private Long seq;
-
-    @Column(length=65, nullable = false)
-    private String gid;
-
-    @Column(length=80, nullable = false, unique = true)
-    private String email;
-
-    @Column(length=40, nullable = false, unique = true)
-    private String userId;
-
-    @Column(length=65, nullable = false)
-    private String password;
-
-    @Column(length=40, nullable = false)
-    private String name;
-
+    @Id //PK
+    @GeneratedValue //자동 키 생성
+    private Long num; //회원 번호
+    @Column(length = 30, nullable = false, unique = true)
+    private String userId; //사용자 아이디
     @Enumerated(EnumType.STRING)
-    @Column(length=10, nullable = false)
-    private Authority authority = Authority.USER;
-
+    private Authority authority; //사용자 구분1(운영자 / 교육자 / 일반학습자 / 학생학습자)
+    @Column(length = 30, nullable = false)
+    private String name; //사용자 성명
+    @Column(length=200, nullable = false)
+    private String password; //비밀번호
+    @Column(nullable = false)
+    private String tel; //전화번호
+    @Column(nullable = false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private String birth; //생년월일
+    @Column
+    private String gender; //성별 (M / F)
+    @Column(length=40, unique = true)
+    private String email; //이메일
     private boolean enable = true;
+    //  @Column(length=30, nullable = false)
+    private Long levels = 1L; //레벨 (??)
+    //  @Column
+//  private String type; //사용자 구분2(유료회원 / 무료회원)
+//  @Column
+//  private String phonNum; //집전화
+    //@Column(name="_lock")
+    //private boolean lock;
+    @Column
+    private boolean agree; //수신동의(이메일 E , SMS 수신 S, 모두 수신 ES, 수신 X)
 
+    @Column
+    private boolean agree2; //수신동의
+
+    @Column
+    private boolean agree3; //수신동의
+
+    @Column
+    private boolean agree4; //수신동의
+
+    @Column
+    private boolean agree5; //수신동의
+
+    @Column
+    private boolean SMSAgree; //수신동의
+
+    @Column
+    private boolean EmailAgree; //수신동의
+//  @Column
+//  private boolean use;  // 계정 상태 (정상 1 / 정지,탈퇴 0)
+
+    ////////////////////////////////////////////////
+/*
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<StudyGroup> StudyGroups;
+
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<GameContent> gameContents;
+
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<EduData> eduDatas;
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<Notice_> notices;
+
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<Homework> homeworks;
+
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<TrainingData> trainingDatas;
+
+  @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+  private List<JoinStudyGroup> joinStudyGroups;
+  */
 }
