930c70cc57bafc7276221ccf343dcfb5e9efa925
cbmja <cbmja@naver.com>
2024-02-23 10:31:48 +0900

    스터디그룹

--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -33,7 +33,7 @@ class ProjectApplicationTests {
             member4.setEnable(true);
             member4.setSMSAgree(true);
             member4.setLevels(100L);
-            member4.setUserId("student02");
+            member4.setUserId("teacher01");
             member4.setEmail("cbmffjghggha@naver.com");
             member4.setPassword(encoder.encode("_aA123456"));
             member4.setAuthority(Authority.TEACHER);
@@ -43,6 +43,66 @@ class ProjectApplicationTests {
 
             joinService.process(member4);
 
+            Member member3 = new Member();
+            member4.setName("홍길동");
+            member4.setAgree(true);
+            member4.setAgree2(true);
+            member4.setAgree3(true);
+            member4.setAgree4(true);
+            member4.setAgree5(true);
+            member4.setEmailAgree(true);
+            member4.setEnable(true);
+            member4.setSMSAgree(true);
+            member4.setLevels(100L);
+            member4.setUserId("teacher02");
+            member4.setEmail("cbmffjghgg3ha@naver.com");
+            member4.setPassword(encoder.encode("_aA123456"));
+            member4.setAuthority(Authority.TEACHER);
+            member4.setBirth("1999-01-01");
+            member4.setGender("M");
+            member4.setTel("01001010101");
+
+            joinService.process(member3);
+            Member member45 = new Member();
+            member4.setName("홍길동");
+            member4.setAgree(true);
+            member4.setAgree2(true);
+            member4.setAgree3(true);
+            member4.setAgree4(true);
+            member4.setAgree5(true);
+            member4.setEmailAgree(true);
+            member4.setEnable(true);
+            member4.setSMSAgree(true);
+            member4.setLevels(100L);
+            member4.setUserId("student01");
+            member4.setEmail("cbmffjjghggha@naver.com");
+            member4.setPassword(encoder.encode("_aA123456"));
+            member4.setAuthority(Authority.STUDENT);
+            member4.setBirth("1999-01-01");
+            member4.setGender("M");
+            member4.setTel("01001010101");
+
+            joinService.process(member45);
+            Member member44 = new Member();
+            member4.setName("홍길동");
+            member4.setAgree(true);
+            member4.setAgree2(true);
+            member4.setAgree3(true);
+            member4.setAgree4(true);
+            member4.setAgree5(true);
+            member4.setEmailAgree(true);
+            member4.setEnable(true);
+            member4.setSMSAgree(true);
+            member4.setLevels(100L);
+            member4.setUserId("student02");
+            member4.setEmail("cbmffjjtghggha@naver.com");
+            member4.setPassword(encoder.encode("_aA123456"));
+            member4.setAuthority(Authority.STUDENT);
+            member4.setBirth("1999-01-01");
+            member4.setGender("M");
+            member4.setTel("01001010101");
+
+            joinService.process(member44);
 
 
 
