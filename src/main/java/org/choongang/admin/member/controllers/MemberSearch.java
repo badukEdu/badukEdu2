049e82460e800a8c509c14ac79315e8765664483
kwi5124 <kwi5124@naver.com>
2024-02-26 12:14:01 +0900

    회원관리 및 조회

--- src/main/java/org/choongang/admin/member/controllers/MemberSearch.java
+++ src/main/java/org/choongang/admin/member/controllers/MemberSearch.java
@@ -7,6 +7,11 @@ public class MemberSearch {
     private int page = 1;
     private int limit = 20;
 
+    private String userId;
+    private String name;
+    private String tel;
+    private String email;
+
     private String sopt;
     private String skey;
 }
